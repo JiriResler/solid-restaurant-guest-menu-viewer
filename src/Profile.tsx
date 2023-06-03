ee814ce6e894a45098abba255fa99f4a7522b305
Jiří Resler <j.resler.29@gmail.com>
2023-06-03 12:02:04 +0200

    add filtering of menu items
    testing basic functionality

--- src/Profile.tsx
+++ src/Profile.tsx
@@ -26,32 +26,45 @@ import { SCHEMA_INRUPT, RDF, AS } from "@inrupt/vocab-common-rdf";
 const Profile: React.FC = () => {
   const { session } = useSession();
 
-  async function handleReadMenu() {
-    const podsUrls: String[] = await getPodUrlAll(session.info.webId, { fetch: session.fetch });
+  async function showPersonalizedMenu() {
+    // menu
     const readingListUrl = 'https://coolrestaurant.solidweb.org/public/menus/my-menu1';
 
     const savedReadingList = await getSolidDataset(readingListUrl, { fetch: session.fetch });
 
-    let item = getThing(savedReadingList, 'https://coolrestaurant.solidweb.org/public/menus/my-menu1#ingredient1');
+    let item1 = getThing(savedReadingList, 'https://coolrestaurant.solidweb.org/public/menus/my-menu1#item1');
 
-    alert("Menu: " + getStringNoLocale(item, SCHEMA_INRUPT.name));
-  }
+    let item2 = getThing(savedReadingList, 'https://coolrestaurant.solidweb.org/public/menus/my-menu1#item2');
 
-  async function handleReadProfile() {
+    // alert(item1.url + getStringNoLocale(item1, SCHEMA_INRUPT.name) + item2.url + getStringNoLocale(item2, SCHEMA_INRUPT.name));
+
+    // profile
     const podsUrls: String[] = await getPodUrlAll(session.info.webId, { fetch: session.fetch });
-    const readingListUrl = `${podsUrls[0]}dietary-profile/my-profile`;
 
-    const savedReadingList = await getSolidDataset(readingListUrl, { fetch: session.fetch });
+    const readingListUrl2 = `${podsUrls[0]}dietary-profile/my-profile`;
 
-    let item = getThing(savedReadingList, `${podsUrls[0]}dietary-profile/my-profile#title`);
+    const savedReadingList2 = await getSolidDataset(readingListUrl2, { fetch: session.fetch });
 
-    alert("Profile: " + getStringNoLocale(item, SCHEMA_INRUPT.name));
-  }  
+    let item3 = getThing(savedReadingList2, `${podsUrls[0]}dietary-profile/my-profile#user`);
+
+    // alert("Profile: " + getStringNoLocale(item3, SCHEMA_INRUPT.name));
+
+    let menuItem1Allergen = getStringNoLocale(item1, SCHEMA_INRUPT.name);
+    let menuItem2Allergen = getStringNoLocale(item2, SCHEMA_INRUPT.name);
+
+    let userAllergen = getStringNoLocale(item3, SCHEMA_INRUPT.name);
+
+    let canUserEatItem1: boolean = userAllergen !== menuItem1Allergen;
+    let canUserEatItem2: boolean = userAllergen !== menuItem2Allergen;
+    
+    alert("canUserEatItem1: " + canUserEatItem1 + " canUserEatItem2: " + canUserEatItem2);
+  } 
 
   return (
     <>
       <h1>Welcome to the Solid personalized restaurant guest menu viewer</h1>
-      <button onClick={() => {handleReadMenu(); handleReadProfile();}}>Read menu and profile</button>
+      <p>Logged in: {session.info.webId}</p>
+      <button onClick={() => {showPersonalizedMenu();}}>Show personalized menu</button>
     </>
   );
 };
