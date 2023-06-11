57c7392fc5b6f93e13533a3959c8f145d229aeb0
Jiří Resler <j.resler.29@gmail.com>
2023-06-11 09:41:55 +0200

    add loading profile and menu mockup

--- src/Profile.tsx
+++ src/Profile.tsx
@@ -1,79 +1,47 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import {
   useSession,
 } from "@inrupt/solid-ui-react";
-
-import {
-  addUrl,
-  addStringNoLocale,
-  createSolidDataset,
-  createThing,
-  getPodUrlAll,
-  getSolidDataset,
-  getThingAll,
-  getStringNoLocale,
-  removeThing,
-  saveSolidDatasetAt,
-  setThing,
-  SolidDataset,
-  getThing
-} from "@inrupt/solid-client";
-
-import { SCHEMA_INRUPT, RDF, AS } from "@inrupt/vocab-common-rdf";
 
 
 const Profile: React.FC = () => {
   const { session } = useSession();
 
-  async function showPersonalizedMenu() {
-    // menu
-    const menuWebId = 'https://coolrestaurant.solidweb.org/profile/card#me';
-    const podsUrls: String[] = await getPodUrlAll(menuWebId, { fetch: session.fetch });
-    
-    const restaurantPodUrl = podsUrls[0];
-    const readingListUrl = restaurantPodUrl + 'public/menus/my-menu1';
+  // list of strings
+  const [guestAllergens, setGuestAllergens] = useState([]);
 
-    const savedReadingList = await getSolidDataset(readingListUrl, { fetch: session.fetch });
+  const [profileLoaded, setProfileLoaded] = useState(false);
 
-    let item1 = getThing(savedReadingList, restaurantPodUrl + 'public/menus/my-menu1#item1');
+  const [menuLoaded, setMenuLoaded] = useState(false);
 
-    let item2 = getThing(savedReadingList, restaurantPodUrl + 'public/menus/my-menu1#item2');
-
-    // alert(item1.url + getStringNoLocale(item1, SCHEMA_INRUPT.name) + item2.url + getStringNoLocale(item2, SCHEMA_INRUPT.name));
-
-    // profile
-    const podsUrls2: String[] = await getPodUrlAll(session.info.webId, { fetch: session.fetch });
-
-    const readingListUrl2 = `${podsUrls2[0]}dietary-profile/my-profile`;
-
-    const savedReadingList2 = await getSolidDataset(readingListUrl2, { fetch: session.fetch });
-
-    let item3 = getThing(savedReadingList2, `${podsUrls2[0]}dietary-profile/my-profile#user`);
-
-    // alert("Profile: " + getStringNoLocale(item3, SCHEMA_INRUPT.name));
-
-    let menuItem1Allergen = getStringNoLocale(item1, SCHEMA_INRUPT.name);
-    let menuItem2Allergen = getStringNoLocale(item2, SCHEMA_INRUPT.name);
-
-    let userAllergen = getStringNoLocale(item3, SCHEMA_INRUPT.name);
-
-    let canUserEatItem1: boolean = userAllergen !== menuItem1Allergen;
-    let canUserEatItem2: boolean = userAllergen !== menuItem2Allergen;
-
-    const resultIsCorrect = !canUserEatItem1 && canUserEatItem2
-    
-    // alert("canUserEatItem1: " + canUserEatItem1 + " canUserEatItem2: " + canUserEatItem2);
-
-    const resultMessage = resultIsCorrect ? "Success: The result is correct" : "Failure: The result is not correct, something is wrong, take a look at it.";
-    alert(resultMessage);
-  } 
+  async function loadProfile() {
+    setGuestAllergens(['al1']);
+    setProfileLoaded(true);
+  }
 
   return (
     <>
-      <h1>Welcome to the Solid personalized restaurant guest menu viewer</h1>
-      <p>Logged in: {session.info.webId}</p>
-      <button onClick={() => {showPersonalizedMenu();}}>Show personalized menu</button>
+      <h1>Solid personalized menu viewer</h1>
+      <p>Logged in as {session.info.webId}</p>
+      <button onClick={() => loadProfile()}>Load profile</button>
+      <br /><br />
+      <p>You are allergic to: {guestAllergens}</p>
+      <button onClick={() => setMenuLoaded(true)}>Load a restaurant menu</button>
+      {menuLoaded &&
+        <>
+          <br /><br />
+          <h2>Menu</h2>
+          <p />
+          <h3 style={{color: 'orange'}}>Item1</h3>
+          <p><b>Ingredient1, ingredient2</b></p>
+          <p>Contains: <p style={{color: 'red'}}>allergen1</p></p>
+          <hr />
+          <h3 style={{color: 'green'}}>Item2</h3>
+          <p><b>Ingredient2, ingredient3</b></p>
+          <p>Contains: allergen2</p>
+        </>
+      }
+
     </>
   );
 };
